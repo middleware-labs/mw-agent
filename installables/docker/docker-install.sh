734a649cfc4f4552e88f548af6bce1470f62b676
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2022-07-26 18:01:44 +0530

    Update docker-install.sh

--- installables/docker/docker-install.sh
+++ installables/docker/docker-install.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+docker pull ghcr.io/middleware-labs/agent-host-go:dev
+docker run -e TARGET=http://localhost:4317 -e MELT_API_KEY=avwisahcge0rx1hukcg4msbmlgcd2lijyx05 -d --name=MELT_Agent_go --restart always --network=host ghcr.io/middleware-labs/agent-host-go:dev
