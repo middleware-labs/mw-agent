0350b8e69c411bb6100eb59ccdcadaf051b07027
ACQ053 <keval@middleware.io>
2022-09-01 12:36:33 +0530

    meltgoservice => meltservice

--- installables/apt/apt-uninstall.sh
+++ installables/apt/apt-uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo systemctl stop meltgoservice
-sudo systemctl disable meltgoservice
+sudo systemctl stop meltservice
+sudo systemctl disable meltservice
 
 sudo apt-get remove mw-go-agent-host -y
 
 sudo rm -rf /usr/local/bin/mw-go-agent/apt
-sudo rm -rf /etc/systemd/system/meltgoservice.service
+sudo rm -rf /etc/systemd/system/meltservice.service
 sudo rm -rf /etc/apt/sources.list.d/mw-go.list
 sudo rm -rf /var/lib/apt/lists/host.middleware.io*
 sudo apt-get clean
