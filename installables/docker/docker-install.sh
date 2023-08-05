34e7dfbdb56fc9aaa0084d8a97e21304a3463552
Keval <keval@middleware.io>
2023-08-05 18:33:49 +0530

    updating APT + RPM workflow to make linux builds easy

--- installables/docker/docker-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker pull ghcr.io/middleware-labs/agent-host-go:dev
-docker run -d \
---pid host \
---restart always \
--v /var/run/docker.sock:/var/run/docker.sock \
--e MW_API_KEY=$MW_API_KEY \
--e TARGET=$TARGET \
---network=host ghcr.io/middleware-labs/agent-host-go:dev api-server start
