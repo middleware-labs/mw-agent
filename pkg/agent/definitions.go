f0bf3a2a2a4d13b7537d56fcbfcf7850a108d4cb
Tejas <tejas@middleware.io>
2023-12-20 16:50:28 -0800

    Add capability to autodetect config check url from target

--- pkg/agent/definitions.go
+++ pkg/agent/definitions.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"strings"
 
 	"go.opentelemetry.io/collector/otelcol"
 )
@@ -24,6 +25,10 @@ const (
 	Service                = "service"
 	Pipelines              = "pipelines"
 	Metrics                = "metrics"
+)
+
+var (
+	ErrInvalidTarget = fmt.Errorf("invalid target")
 )
 
 // InfraPlatform defines the agent's infrastructure platform
@@ -135,4 +140,26 @@ func getHostname() string {
 		return ""
 	}
 	return hostname
+}
+
+func GetAPIURLForConfigCheck(target string) (string, error) {
+	url := strings.TrimRight(target, "/")
+
+	// There should at least be two "." in the URL
+	parts := strings.Split(url, ".")
+	if len(parts) < 3 {
+		return "", ErrInvalidTarget
+	}
+
+	// Find the index of the last "/" and first "."
+	firstSlash := strings.LastIndex(url, "/")
+	firstDot := strings.Index(url, ".")
+
+	// Check if both "/" and "." exist in the URL
+	if firstSlash != -1 && firstDot != -1 {
+		// Replace the string between "/" and the first "."
+		return url[:firstSlash+1] + "app" + url[firstDot:], nil
+	}
+
+	return "", ErrInvalidTarget
 }
