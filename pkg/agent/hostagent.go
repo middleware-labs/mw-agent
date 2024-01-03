cfdfe501ff187e6dcbe756d4ff35cdd9a539dbf6
Pradyuman <145964996+ppradyu@users.noreply.github.com>
2024-01-03 16:36:49 +0530

    FIX: YAML not being updated when agent restarts
    * FIX: YAML not being updated when agent restarts
    
    * FIX: Error Handling
    
    * FIX: Missing return statement
    
    * FIX: Typo

--- pkg/agent/hostagent.go
+++ pkg/agent/hostagent.go
@@ -408,6 +408,10 @@ func (c *HostAgent) callRestartStatusAPI() error {
 
 	if apiResponse.Restart {
 		c.logger.Info("restarting mw-agent")
+		if _, err := c.GetUpdatedYAMLPath(); err != nil {
+			c.logger.Error("error getting Updated YAML", zap.Error(err))
+			return err
+		}
 		if err := restartHostAgent(); err != nil {
 			c.logger.Error("error restarting mw-agent", zap.Error(err))
 			return err
