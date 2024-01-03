d939f34aac0b4447fbdbe4ea7950621629f8add2
ppradyu <pradyuman@middleware.io>
2024-01-03 16:30:00 +0530

    FIX: Missing return statement

--- pkg/agent/hostagent.go
+++ pkg/agent/hostagent.go
@@ -410,6 +410,7 @@ func (c *HostAgent) callRestartStatusAPI() error {
 		c.logger.Info("restarting mw-agent")
 		if _, err := c.GetUpdatedYAMLPath(); err != nil {
 			c.logger.Error("Error getting Updated YAML", zap.Error(err))
+			return err
 		}
 		if err := restartHostAgent(); err != nil {
 			c.logger.Error("error restarting mw-agent", zap.Error(err))
