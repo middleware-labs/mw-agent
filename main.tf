af8bee341928b1d7fe71777732366619c89cbfac
meealmathur23 <meenalmathur23@gmail.com>
2024-04-03 11:14:49 +0530

    docker terraform script

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "digitalocean_droplet" "example" {
 provisioner "remote-exec" {
   inline = [
     "sleep 60",
-    'sudo apt-get update',
-    'MW_AGENT_DOCKER_IMAGE=ghcr.io/middleware-labs/terraform-agent MW_API_KEY=e3g4p2dkmoee09mmlwql64q2oornpyupht4c MW_TARGET=https://gh5k31l.middleware.io:443 bash -c "$(curl -L https://install.middleware.io/scripts/docker-install.sh)"',
+    "sudo apt-get update",
+    "MW_AGENT_DOCKER_IMAGE=ghcr.io/middleware-labs/terraform-agent MW_API_KEY=e3g4p2dkmoee09mmlwql64q2oornpyupht4c MW_TARGET=https://gh5k31l.middleware.io:443 bash -c '$(curl -L https://install.middleware.io/scripts/docker-install.sh)'",
   ]
 }
