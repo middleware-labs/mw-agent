fa0f5c8e0ef8435ab01f2567e09e5cf8cfd56e6c
meealmathur23 <meenalmathur23@gmail.com>
2024-04-03 10:40:59 +0530

    docker terraform script

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "digitalocean_droplet" "example" {
+  name   = "terraform-droplet"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  image  = "ubuntu-20-04-x64"
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sleep 60",
+    'sudo apt-get update',
+    'MW_AGENT_DOCKER_IMAGE=ghcr.io/middleware-labs/terraform-agent MW_API_KEY=e3g4p2dkmoee09mmlwql64q2oornpyupht4c MW_TARGET=https://gh5k31l.middleware.io:443 bash -c "$(curl -L https://install.middleware.io/scripts/docker-install.sh)"',
+  ]
+}
