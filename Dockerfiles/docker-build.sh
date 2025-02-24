c721c1116bc67122bec5ed240d2d1b506cdd4aca
Tejas Kokje <tejas@middleware.io>
2025-02-24 14:23:39 -0800

    Add kubernetes agent config updater

--- Dockerfiles/docker-build.sh
+++ Dockerfiles/docker-build.sh
@@ -4,10 +4,18 @@ then
       echo "please pass target as 1st argument"
       exit
 fi
+
 if [ -z "$2" ]
 then
-      echo "please pass release tag as 2nd argument"
+      echo "please pass image name as 2nd argument"
       exit
 fi
-docker build . --target $1 -t ghcr.io/middleware-labs/mw-agent:$2 -f $3
+
+if [ -z "$3" ]
+then
+      echo "please pass release tag as 3rd argument"
+      exit
+fi
+
+docker build .  --target $1 -t $2:$3 -f $4
 
