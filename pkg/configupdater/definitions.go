cb975b62d1e40d2d25bf1e496c493769821b878a
Tejas Kokje <136300685+tejaskokje-mw@users.noreply.github.com>
2025-04-07 11:21:40 -0700

    Add kubernetes agent config updater (#230)
    * Add kubernetes agent config updater
    
    * Add workflow file for kube-agent-config-updater

---
+++ pkg/configupdater/definitions.go
@@ -0,0 +1,170 @@
+package configupdater
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"go.uber.org/zap"
+	"k8s.io/client-go/kubernetes"
+)
+
+// Otel config components
+const (
+	Receivers              = "receivers"
+	AWSECSContainerMetrics = "awsecscontainermetrics"
+	Service                = "service"
+	Pipelines              = "pipelines"
+	Metrics                = "metrics"
+)
+
+var (
+	ErrInvalidTarget = fmt.Errorf("invalid target")
+)
+
+// InfraPlatform defines the agent's infrastructure platform
+type InfraPlatform uint16
+
+var (
+	// InfraPlatformInstance is for bare metal or VM platform
+	InfraPlatformInstance InfraPlatform = 0
+	// InfraPlatformKubernetes is for Kubernetes platform
+	InfraPlatformKubernetes InfraPlatform = 1
+	// InfraPlatformECSEC2 is for AWS ECS EC2 platform
+	InfraPlatformECSEC2 InfraPlatform = 2
+	// InfraPlatformECSFargate is for AWS ECS Fargate platform
+	InfraPlatformECSFargate InfraPlatform = 3
+	// InfraPlatformCycleIO is for Cycle.io platform
+	InfraPlatformCycleIO InfraPlatform = 4
+)
+
+func (p InfraPlatform) String() string {
+	switch p {
+	case InfraPlatformInstance:
+		return "instance"
+	case InfraPlatformKubernetes:
+		return "kubernetes"
+	case InfraPlatformECSEC2:
+		return "ecsec2"
+	case InfraPlatformECSFargate:
+		return "ecsfargate"
+	case InfraPlatformCycleIO:
+		return "cycleio"
+	}
+	return "unknown"
+}
+
+type configType struct {
+	Docker     map[string]interface{} `json:"docker"`
+	NoDocker   map[string]interface{} `json:"nodocker"`
+	Deployment map[string]interface{} `json:"deployment"`
+	DaemonSet  map[string]interface{} `json:"daemonset"`
+}
+
+var (
+	apiPathForYAML    = "api/v1/agent/ingestion-rules"
+	apiPathForRestart = "api/v1/agent/restart-status"
+)
+
+type apiResponseForYAML struct {
+	Status  bool       `json:"status"`
+	Config  configType `json:"config"`
+	Message string     `json:"message"`
+}
+
+type rollout struct {
+	Deployment bool `json:"deployment"`
+	Daemonset  bool `json:"daemonset"`
+}
+
+type apiResponseForRestart struct {
+	Status  bool    `json:"status"`
+	Restart bool    `json:"restart"`
+	Rollout rollout `json:"rollout"`
+	Message string  `json:"message"`
+}
+
+type Client struct {
+	APIURLForConfigCheck string
+	APIKey               string
+	Target               string
+	InfraPlatform        InfraPlatform
+}
+
+type AgentFeatures struct {
+	MetricCollection    bool
+	LogCollection       bool
+	SyntheticMonitoring bool
+}
+
+// String() implements stringer interface for KubeAgentUpdaterConfig
+func (c KubeAgent) String() string {
+	var s string
+	s += fmt.Sprintf("api-key: %s, ", c.APIKey)
+	s += fmt.Sprintf("target: %s, ", c.Target)
+	s += fmt.Sprintf("enable-synthetic-monitoring: %t, ", c.EnableSyntheticMonitoring)
+	s += fmt.Sprintf("config-check-interval: %s, ", c.ConfigCheckInterval)
+	s += fmt.Sprintf("api-url-for-config-check: %s, ", c.APIURLForConfigCheck)
+	s += fmt.Sprintf("daemonset-name: %s, ", c.DaemonsetName)
+	s += fmt.Sprintf("daemonset-configmap-name: %s, ", c.DaemonsetConfigMapName)
+	s += fmt.Sprintf("deployment-name: %s, ", c.DeploymentName)
+	s += fmt.Sprintf("deployment-configmap-name: %s, ", c.DeploymentConfigMapName)
+	return s
+}
+
+type BaseConfig struct {
+	APIKey                    string
+	Target                    string
+	EnableSyntheticMonitoring bool
+	ConfigCheckInterval       string
+	APIURLForConfigCheck      string
+	AgentNamespaceName        string
+	DaemonsetName             string
+	DaemonsetConfigMapName    string
+	DeploymentName            string
+	DeploymentConfigMapName   string
+	ClusterName               string
+}
+
+// KubeConfig stores configuration for all the host agent
+type KubeAgent struct {
+	BaseConfig
+	clientset           kubernetes.Interface
+	configCheckDuration time.Duration
+	logger              *zap.Logger
+	version             string
+}
+
+func GetAPIURLForConfigCheck(target string) (string, error) {
+
+	// There should at least be two "." in the URL
+	parts := strings.Split(target, ".")
+	if len(parts) < 3 {
+		return "", ErrInvalidTarget
+	}
+
+	return strings.TrimSuffix(target, "/"), nil
+}
+
+// GetAPIURLForSyntheticMonitoring constructs the WebSocket URL for synthetic monitoring
+func GetAPIURLForSyntheticMonitoring(target string) (string, error) {
+	// Parse the URL
+	parsedURL, err := url.Parse(target)
+	if err != nil {
+		return "", err
+	}
+
+	// Check if the host part of the URL contains more than one '.'
+	hostParts := strings.Split(parsedURL.Hostname(), ".")
+	if len(hostParts) < 3 {
+		return "", ErrInvalidTarget
+	}
+
+	// Ensure no trailing slash in the path
+	trimmedURL := strings.TrimSuffix(parsedURL.Host, "/")
+
+	// Build the WebSocket URL
+	webSocketURL := "wss://" + trimmedURL + "/plsrws/v2"
+	return webSocketURL, nil
+}
---
+++ pkg/configupdater/kubeagentupdater_test.go
@@ -0,0 +1,218 @@
+package configupdater
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+	kubetesting "k8s.io/client-go/testing"
+)
+
+func TestListenForKubeOtelConfigChanges(t *testing.T) {
+	var cfg BaseConfig
+	cfg.ConfigCheckInterval = "1s"
+	cfg.APIURLForConfigCheck = "http://example.com"
+	cfg.APIKey = "apikey"
+	cfg.ClusterName = "cluster"
+	agent, err := NewKubeAgent(cfg, "0.0.1", fake.NewClientset(), nil)
+	assert.NoError(t, err)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Start listening for config changes in a separate goroutine
+	go func() {
+		err := agent.ListenForConfigChanges(ctx, make(chan error), make(chan struct{}))
+		assert.NoError(t, err)
+	}()
+
+	// Wait for a short period to allow the goroutine to start
+	time.Sleep(100 * time.Millisecond)
+
+	// Manually cancel the context to stop listening for config changes
+	cancel()
+
+	// Wait for a short period to allow the goroutine to stop
+	time.Sleep(100 * time.Millisecond)
+
+	// Assert that the goroutine has stopped and no error occurred
+	assert.True(t, true)
+}
+
+func TestCallRestartStatusAPI(t *testing.T) {
+	// Create a KubeAgentMonitor instance with mocked dependencies
+	zapEncoderCfg := zapcore.EncoderConfig{
+		MessageKey:   "message",
+		LevelKey:     "level",
+		EncodeLevel:  zapcore.CapitalLevelEncoder,
+		TimeKey:      "time",
+		EncodeTime:   zapcore.ISO8601TimeEncoder,
+		CallerKey:    "caller",
+		EncodeCaller: zapcore.ShortCallerEncoder,
+	}
+	zapCfg := zap.NewProductionConfig()
+	zapCfg.EncoderConfig = zapEncoderCfg
+	logger, _ := zapCfg.Build()
+
+	c := &KubeAgent{
+		BaseConfig: BaseConfig{
+			APIURLForConfigCheck: "http://example.com",
+			APIKey:               "apikey",
+			ClusterName:          "cluster",
+		},
+
+		logger: logger,
+	}
+
+	// Mock the HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/restart" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		http.NotFound(w, r)
+	}))
+	defer server.Close()
+
+	// Override the API URL with the mock server's URL
+	c.APIURLForConfigCheck = server.URL
+
+	server.Config.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a map representing the JSON response body
+		responseBody := map[string]interface{}{
+			"key": "value",
+		}
+
+		// Encode the response body to JSON
+		jsonResponse, err := json.Marshal(responseBody)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the Content-Type header to indicate JSON content
+		w.Header().Set("Content-Type", "application/json")
+
+		// Write the JSON response with a 200 status code
+		w.WriteHeader(http.StatusOK)
+		_, errWrite := w.Write(jsonResponse)
+		if errWrite != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	// Test case for successful API call
+	err := c.callRestartStatusAPI(context.Background())
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	// Test case for API error
+	server.Config.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+	err = c.callRestartStatusAPI(context.Background())
+	if err == nil {
+		t.Errorf("Expected an error, got nil")
+	}
+}
+
+func TestRolloutRestart(t *testing.T) {
+
+	// Create a fake clientset for testing
+	fakeClientset := fake.NewSimpleClientset()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Sample daemonset
+	daemonset := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-daemonset",
+			Namespace: "test-namespace",
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{},
+				},
+			},
+		},
+	}
+
+	_, errDaemonset := fakeClientset.AppsV1().DaemonSets("test-namespace").Create(ctx, daemonset, metav1.CreateOptions{})
+	if errDaemonset != nil {
+		return
+	}
+
+	kubeAgentMonitor := &KubeAgent{
+		BaseConfig: BaseConfig{
+			APIURLForConfigCheck: "http://example.com",
+			APIKey:               "apikey",
+			ClusterName:          "cluster",
+			AgentNamespaceName:   "test-namespace",
+			DaemonsetName:        "test-daemonset",
+			DeploymentName:       "test-deployment",
+		},
+
+		clientset: fakeClientset,
+	}
+
+	// Mocking the DaemonSet update
+	fakeClientset.PrependReactor("update", "daemonsets", func(action kubetesting.Action) (handled bool, ret runtime.Object, err error) {
+		if updateAction, ok := action.(kubetesting.UpdateActionImpl); ok {
+			obj := updateAction.GetObject()
+			return true, obj, nil
+		}
+		return false, nil, nil
+	})
+
+	// Call rolloutRestart for DaemonSet
+	err := kubeAgentMonitor.rolloutRestart(context.Background(), DaemonSet)
+	assert.NoError(t, err)
+
+	// Sample deployment
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-deployment",
+			Namespace: "test-namespace",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{},
+				},
+			},
+		},
+	}
+
+	_, errDeployment := fakeClientset.AppsV1().Deployments("test-namespace").Create(ctx, deployment, metav1.CreateOptions{})
+	if errDeployment != nil {
+		return
+	}
+
+	// Mocking the DaemonSet update
+	fakeClientset.PrependReactor("update", "deployments", func(action kubetesting.Action) (handled bool, ret runtime.Object, err error) {
+		if updateAction, ok := action.(kubetesting.UpdateActionImpl); ok {
+			obj := updateAction.GetObject()
+			return true, obj, nil
+		}
+		return false, nil, nil
+	})
+
+	// Call rolloutRestart for DaemonSet
+	err = kubeAgentMonitor.rolloutRestart(context.Background(), Deployment)
+	assert.NoError(t, err)
+}
