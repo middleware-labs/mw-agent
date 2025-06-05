98bff3f4c3f43cd21ea03c6dfb09b55366f3d2e0
milan03-mw <milan.vaghasiya@middleware.io>
2025-06-05 16:28:16 +0530

    AGE-233: - Correct agent checking cli function

--- integrations/utils/linux_commands.go
+++ integrations/utils/linux_commands.go
@@ -1,25 +1,11 @@
 package utils
 
 import (
-	"bytes"
 	"os/exec"
-	"strings"
 )
 
 func IsMwAgentRunning() bool {
-	cmd := exec.Command("pgrep", "-fl", "mw-agent")
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	cmd := exec.Command("pidof", "mw-agent")
 	err := cmd.Run()
-	if err != nil {
-		return false
-	}
-
-	lines := strings.Split(out.String(), "\n")
-	for _, line := range lines {
-		if strings.Contains(line, "mw-agent") && !strings.Contains(line, "grep") {
-			return true
-		}
-	}
-	return false
+	return err == nil
 }
