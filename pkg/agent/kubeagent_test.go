357fdd26bb6bff52b8eb00b99d0dc1475f6f9941
Hardik Choksi <writetohardikchoksi@gmail.com>
2026-02-18 12:41:09 +0530

    impr: update tests incorporating zookeeper receiver changes

--- pkg/agent/kubeagent_test.go
+++ pkg/agent/kubeagent_test.go
@@ -36,7 +36,7 @@ func TestKubeAgentGetFactories(t *testing.T) {
 	assert.Len(t, factories.Extensions, 1)
 
 	// check if factories contains expected receivers
-	assert.Len(t, factories.Receivers, 20)
+	assert.Len(t, factories.Receivers, 21)
 	assertContainsComponent(t, factories.Receivers, "otlp")
 	assertContainsComponent(t, factories.Receivers, "fluentforward")
 	assertContainsComponent(t, factories.Receivers, "filelog")
@@ -55,6 +55,7 @@ func TestKubeAgentGetFactories(t *testing.T) {
 	assertContainsComponent(t, factories.Receivers, "mongodbatlas")
 	assertContainsComponent(t, factories.Receivers, "postgresql")
 	assertContainsComponent(t, factories.Receivers, "elasticsearch")
+	assertContainsComponent(t, factories.Receivers, "zookeeper")
 
 	// check if factories contain expected exporters
 	assert.Len(t, factories.Exporters, 4)
